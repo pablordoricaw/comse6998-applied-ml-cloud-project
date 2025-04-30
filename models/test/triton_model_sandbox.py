e5791f9b588ed6142dfd34616624c3adb8b6d5ce
Pablo Ordorica Wiener <pablo_ordoricaw@hotmail.com>
2025-04-30 14:18:18 -0400

    chore(models-test): remove unused dependency yaspin

--- models/test/triton_model_sandbox.py
+++ models/test/triton_model_sandbox.py
@@ -2,7 +2,6 @@
 # requires-python = ">=3.10"
 # dependencies = [
 #     "docker",
-#     "yaspin",
 # ]
 # ///
 
@@ -11,7 +10,6 @@ import os
 import signal
 import sys
 import docker
-from yaspin import yaspin
 
 
 def create_argparser():
